3d929edfd9de3fd619430e3f4618ad311984878b
jinho4124 <jh.yoon@sparta-devcamp.com>
2024-07-11 17:42:31 +0900

    feat: s3 파일 업로드

--- seedo/config/settings.py
+++ seedo/config/settings.py
@@ -151,39 +151,18 @@ DJANGO_ENVIRONMENT = env("DJANGO_ENVIRONMENT")
 if DJANGO_ENVIRONMENT == "development":
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
     MEDIA_URL = "/media/"
-    # AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-    # AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-    # AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-    # AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
-
-    # AWS_S3_OBJECT_PARAMETERS = {
-    #     "CacheControl": "max-age=86400",
-    # }
-
-    # AWS_MEDIA_LOCATION = "media"
-
-    # STORAGES = {
-    #     "default": {
-    #         "BACKEND": "storages.backends.s3.S3Storage",
-    #     },
-    #     "staticfiles": {
-    #         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    #     },
-    # }
-    # MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/{AWS_MEDIA_LOCATION}/"
 
 else:
-    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+    # S3 Settings for Media Files
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
 
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-
     AWS_MEDIA_LOCATION = "media"
 
-    STORAGES = {"default": {"BACKEND": "storages.backends.s3.S3Storage"}, "staticfiles": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
-
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/{AWS_MEDIA_LOCATION}/"
 
 
