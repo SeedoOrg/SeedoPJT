7cddb6e51e364731ed98bcc6749e6a074f2bedbd
Elish <kangspa98@naver.com>
2024-07-14 18:06:41 +0900

    feat:front_walk

--- seedo/walking_mode/urls.py
+++ seedo/walking_mode/urls.py
@@ -1,12 +1,19 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render
 from django.urls import path
 
 from .views import ImageUploadView
 
+
+# @token_required
+def index(request):
+    return render(request, "walking_mode/index.html")
+
+
 app_name = "walking_mode"
 
-urlpatterns = [path("", ImageUploadView.as_view(), name="test")]
+urlpatterns = [path("test/", ImageUploadView.as_view(), name="test"), path("", index, name="index")]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
